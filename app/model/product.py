5370d78acd8ba08c4d1589dd5fedf46d107e1933
silvinecoder <creativeservicepackages@gmail.com>
2024-03-16 00:34:28 +0000

    adding categories

--- app/model/product.py
+++ app/model/product.py
@@ -10,5 +10,6 @@ class Product(Base):
     product_uuid = Column(UUID(as_uuid=True), primary_key=True, unique=True, default=uuid.uuid4, nullable=False)
     name = Column(String(255), nullable=False)
     image = Column(String(255), nullable=True)
+    category = Column(String(255), nullable=False)
     supermarket = Column(String(50), nullable=False)
     country = Column(String(50), nullable=False)
