916cf1be8d72212132dade04aedfe495c122a277
silvinecoder <creativeservicepackages@gmail.com>
2024-03-23 16:35:13 +0000

    adding category endpoint and products endpoint

--- app/endpoints/categories.py
+++ app/endpoints/categories.py
@@ -1,27 +1,28 @@
 from flask import Flask, jsonify
 from app.db.db_connection import create_session
+from collections import defaultdict
 from app.model.product import Product
 
 # Create Flask application
 app = Flask(__name__)
 
 # Define route to get all products
-@app.route('/products', methods=['GET'])
-def get_products():
+@app.route('/categories/products', methods=['GET'])
+def get_products_by_category():
     session = create_session()
     products = session.query(Product).all()
     session.close()
 
-    # Convert products to JSON format
-    products_json = []
+    # Group products by category
+    categories_with_products = defaultdict(list)
     for product in products:
-        products_json.append({
+        categories_with_products[product.category].append({
             'product_uuid': str(product.product_uuid),
             'name': product.name,
             'image': product.image,
-            'category': product.category,
             'supermarket': product.supermarket,
             'country': product.country
         })
 
-    return jsonify(products_json), 200
+    return jsonify(categories_with_products), 200
+
