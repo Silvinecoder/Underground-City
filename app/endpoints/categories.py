727a596fa1314768ce986bb87372347b756f78f6
silvinecoder <creativeservicepackages@gmail.com>
2024-03-24 12:47:40 +0000

    adding category endpoint to work with flask locally

--- app/endpoints/categories.py
+++ app/endpoints/categories.py
@@ -1,28 +1,33 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from app.db.db_connection import create_session
 from collections import defaultdict
 from app.model.product import Product
 
 # Create Flask application
 app = Flask(__name__)
+CORS(app)  # Initialize Flask-CORS
 
 # Define route to get all products
 @app.route('/categories/products', methods=['GET'])
 def get_products_by_category():
-    session = create_session()
-    products = session.query(Product).all()
-    session.close()
+    with app.app_context():  # Ensure execution within Flask application context
+        session = create_session()
+        products = session.query(Product).all()
+        session.close()
 
-    # Group products by category
-    categories_with_products = defaultdict(list)
-    for product in products:
-        categories_with_products[product.category].append({
-            'product_uuid': str(product.product_uuid),
-            'name': product.name,
-            'image': product.image,
-            'supermarket': product.supermarket,
-            'country': product.country
-        })
+        # Group products by category
+        categories_with_products = defaultdict(list)
+        for product in products:
+            categories_with_products[product.category].append({
+                'product_uuid': str(product.product_uuid),
+                'name': product.name,
+                'image': product.image,
+                'supermarket': product.supermarket,
+                'country': product.country
+            })
 
-    return jsonify(categories_with_products), 200
+        return jsonify(categories_with_products), 200
 
+if __name__ == "__main__":
+    app.run(debug=True)
