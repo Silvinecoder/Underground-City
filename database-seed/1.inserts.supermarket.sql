664005c4544818f90fee22f13df07db9d2257cc9
silvinecoder <creativeservicepackages@gmail.com>
2024-08-28 22:02:56 +0100

    adding database seed

--- database-seed/1.inserts.supermarket.sql
+++ database-seed/1.inserts.supermarket.sql
@@ -7,13 +7,20 @@ INSERT INTO category (category_uuid, category_name) VALUES
 ('8250b741-8948-471f-9350-bfcc0f37c8d5','Canned'),
 ('a3e179c1-9e9b-4105-94c3-821216551a03','Biscuits');
 
-INSERT INTO product (product_uuid, product_name, product_image, product_price, product_attribute_uuid, product_category_uuid) VALUES
+INSERT INTO product (product_uuid, product_name, product_image, product_price, product_rating, product_attribute_uuid, product_category_uuid) VALUES
 ('b973cb3a-777e-44e4-8a53-f868932f8eb4','Cogumelos Laminados','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dwd9ee9df0/images/col/300/3003826-hero.jpg?sw=2000&sh=2000','€3,65','5ddf34ac-a0ed-4042-869d-d584fd10b97b', '8250b741-8948-471f-9350-bfcc0f37c8d5'),
 ('a8409995-69f4-495a-b801-f71979305d72','Cogumelos Laminados','https://www.pingodoce.pt/wp-content/uploads/products/large/598150-1215f04224bfbf8e878a1dc30a074c10.jpg','€2,65','5ddf34ac-a0ed-4042-869d-d584fd10b97b','8250b741-8948-471f-9350-bfcc0f37c8d5'),
 ('11b31b73-4334-48df-a573-005cac6c4667','Tortitas de Milho com Sal','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw47acd0da/images/col/596/5968600-hero.jpg?sw=2000&sh=2000','€2,65','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
 ('1e78dd01-053f-4a59-aa6d-8f271c6851a1','Tortitas Vitalday de Arroz, Iogurte e Framboesa','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw55d1f831/images/col/718/7188280-frente.jpg?sw=2000&sh=2000','€2,65','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
 ('96e4aacd-1a9f-4e7c-bb07-bcf86a5b548e','Tortitas Vitalday de Arroz, Chocolate e Coco','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw97599dee/images/col/718/7188279-frente.jpg?sw=2000&sh=2000','€2,65','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
 ('ad15e752-9b27-4568-8a63-8615363dce5b','Bolachas de Aveia e Chocolate','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw41acaa99/images/col/770/7708105-esquerdo.jpg?sw=2000&sh=2000', '€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
+('e4011106-aed4-45e4-8b64-d99f26c9b829','Bolachas de Aveia e Chocolate 2','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw41acaa99/images/col/770/7708105-esquerdo.jpg?sw=2000&sh=2000', '€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
+('92dfbad7-0b6d-4061-bbd2-8676bbf62cff','Bolachas de Aveia e Chocolate 3','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw41acaa99/images/col/770/7708105-esquerdo.jpg?sw=2000&sh=2000', '€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
+('a27c2dd3-01ce-4731-b058-bf015a3bb5f1','Bolachas de Aveia e Chocolate 4','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw41acaa99/images/col/770/7708105-esquerdo.jpg?sw=2000&sh=2000', '€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
+('1661fa6a-7def-445f-84e5-49231157f92b','Bolachas de Aveia e Chocolate 5','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw41acaa99/images/col/770/7708105-esquerdo.jpg?sw=2000&sh=2000', '€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
+('b679106b-a967-4a22-9524-25fb24ce4e71','Bolachas de Aveia e Chocolate 6','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw41acaa99/images/col/770/7708105-esquerdo.jpg?sw=2000&sh=2000', '€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
+('c0a11ecd-c778-4064-ab81-5a4e770b290f','Bolachas de Aveia e Chocolate 7','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw41acaa99/images/col/770/7708105-esquerdo.jpg?sw=2000&sh=2000', '€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
+('052ccbae-98fc-40d2-a6d7-332ab235b932','Bolachas de Aveia e Chocolate 8','https://www.continente.pt/dw/image/v2/BDVS_PRD/on/demandware.static/-/Sites-col-master-catalog/default/dw41acaa99/images/col/770/7708105-esquerdo.jpg?sw=2000&sh=2000', '€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03'),
 ('4abe8c2c-ceba-46ce-b3c3-5b3e9da59ffc','bolacha gullon aveia laranja','https://www.auchan.pt/dw/image/v2/BFRC_PRD/on/demandware.static/-/Sites-auchan-pt-master-catalog/default/dwc3407d0b/images/hi-res/002896961.jpg','€1,21','5ddf34ac-a0ed-4042-869d-d584fd10b97b','a3e179c1-9e9b-4105-94c3-821216551a03');
 
 INSERT INTO supermarket (supermarket_uuid, supermarket_name, supermarket_country) VALUES
@@ -30,6 +37,13 @@ INSERT INTO supermarket_product_pair (supermarket_product_pair_uuid, product_uui
 ('a081ea44-f6fa-48ef-8e8a-deb7e6368bee','1e78dd01-053f-4a59-aa6d-8f271c6851a1','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
 ('3c407f25-1920-47df-b90a-366565ca9b33','96e4aacd-1a9f-4e7c-bb07-bcf86a5b548e','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
 ('ebbec397-cb8d-4edc-84a4-159c9509619d','ad15e752-9b27-4568-8a63-8615363dce5b','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
+('db34a430-5a0a-48d0-9009-579eadb08ec6','e4011106-aed4-45e4-8b64-d99f26c9b829','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
+('110ab463-22a6-4cbb-ae7d-68d25feecd8f','92dfbad7-0b6d-4061-bbd2-8676bbf62cff','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
+('1e45d1ee-6343-4ed8-9529-f719a210822d','a27c2dd3-01ce-4731-b058-bf015a3bb5f1','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
+('be05e0de-71ca-48eb-b778-5e81d1fc057e','1661fa6a-7def-445f-84e5-49231157f92b','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
+('3c279334-abe6-488b-9a3d-4c40bf961f04','b679106b-a967-4a22-9524-25fb24ce4e71','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
+('ff43e4eb-e0fd-4110-b6b9-4390ac7579d5','c0a11ecd-c778-4064-ab81-5a4e770b290f','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
+('3182827c-7510-4901-af05-34c07a4444a4','052ccbae-98fc-40d2-a6d7-332ab235b932','c3dc5c28-afc4-4680-9fd6-b39132f82612'),
 ('a2d0fc20-8917-4a29-8376-3be5c0ca73a1','4abe8c2c-ceba-46ce-b3c3-5b3e9da59ffc','058e6b43-4b66-4b7f-b18a-b861830a1703');
 
 INSERT INTO supermarket_category_pair (supermarket_category_pair_uuid, category_uuid, supermarket_uuid) VALUES
